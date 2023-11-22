bc3ecb26d09a0466f0c8bb4db15347e7d1762de0
luszczewskakasia <kasia.luszczewska13@gmail.com>
2023-11-22 19:26:35 +0100

    map fix

--- website/maps/map_fire_in_states/generate_map_Kasia.py
+++ website/maps/map_fire_in_states/generate_map_Kasia.py
@@ -9,14 +9,6 @@ with open("gz_2010_us_040_00_5m_1.json", 'r') as f:
     states = json.load(f)
 
 def fires_in_states():
-    # filtered_df = df[df['month'] == selected_month]
-    # number_to_months = {6: 'June',
-    #                     7: 'July',
-    #                     8: 'August',
-    #                     9: 'September',
-    #                     10: 'October'}
-    #
-    # month_name = number_to_months.get(selected_month)
     state_fires = df.groupby('state')['number of fires'].sum().reset_index()
     df_test = pd.DataFrame(state_fires)
 
@@ -32,8 +24,15 @@ def fires_in_states():
         geo_scope='usa',
     )
 
-    return plotly.offline.plot(fig, filename='fires_in_states.html')
+    fig.update_geos(resolution=110, showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="lightgray")
 
+    graph = fig.to_html(full_html=False, include_plotlyjs='cdn')
+    return graph
 
-if __name__ == '__main__':
-    fires_in_states()
+#
+# if __name__ == '__main__':
+#     fires_in_states()
+
+result = fires_in_states()
+with open("output.html", "w") as f:
+    f.write(result)
