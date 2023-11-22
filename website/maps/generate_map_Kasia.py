5030cd5ed2cc3e9618531242bdfc78fb52f5589e
julitw <julitawjck@gmail.com>
2023-11-22 19:55:08 +0100

    map-fix

--- website/maps/generate_map_Kasia.py
+++ website/maps/generate_map_Kasia.py
@@ -4,11 +4,11 @@ import json
 
 import plotly.graph_objects as go
 
-df = pd.read_csv('gdf_fires.csv')
-with open("gz_2010_us_040_00_5m_1.json", 'r') as f:
+df = pd.read_csv('website\maps\dane_Kasia\gdf_fires.csv')
+with open("website\maps\dane_Kasia\gz_2010_us_040_00_5m_1.json", 'r') as f:
     states = json.load(f)
 
-def fires_in_states():
+def generate_map_Kasia():
     state_fires = df.groupby('state')['number of fires'].sum().reset_index()
     df_test = pd.DataFrame(state_fires)
 
