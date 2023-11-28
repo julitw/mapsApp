1aaaf3544073f17d2dc021b76014f236b80aa5f8
Gabriela Bochenska <gbochenska@student.agh.edu.pl>
2023-11-28 22:49:00 +0100

    map of heart disease

--- website/maps/generate_map_Gabrysia.py
+++ website/maps/generate_map_Gabrysia.py
@@ -1,18 +1,27 @@
-import plotly.express as px
+import pandas as pd
+import json
+import plotly.graph_objects as go
 
+def generate_map_Gabrysia(): 
+    data = pd.read_csv('website\maps\dane_Gabrysia\gdf_diseases.csv')
+    with open("website\maps\dane_Kasia\gz_2010_us_040_00_5m_1.json", 'r') as f:
+        states = json.load(f)
 
-def generate_map_Gabrysia():  # do modyfikacji przez ludzi od map
-    data = [
-        {'City': 'New York', 'Lat': 40.7128, 'Lon': -74.0060, 'Population': 8398748},
-        {'City': 'Los Angeles', 'Lat': 34.0522, 'Lon': -118.2437, 'Population': 3990456},
-        {'City': 'Chicago', 'Lat': 41.8781, 'Lon': -87.6298, 'Population': 2705994},
-    ]
-    fig = px.scatter_geo(data,
-                         lat='Lat',  # szerokość geograficzna
-                         lon='Lon',  # długość geograficzna
-                         text='City',
-                         size='Population',
-                         projection='natural earth')
+    df = pd.DataFrame(data)
+
+    fig = go.Figure(data=go.Choropleth(
+            geojson=states,
+            locations=df['State'],
+            z=df['HeartDisease'],
+            featureidkey="properties.NAME",
+            colorscale='Viridis'
+        ))
+        
+
+    fig.update_layout(
+        title_text=f'Number of people suffering from heart disease',
+        geo_scope='usa',
+        )
 
     fig.update_geos(resolution=110, showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="lightgray")
 
